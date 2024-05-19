60db48c7974165c0b5351dba172e149782313fd8
framer807 <161201303+framer807@users.noreply.github.com>
2024-05-19 00:33:41 -0400

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+def main():
+    data = generate_random_data()
+    return data
+    main()
+if __name__ == "__main__":
+
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
